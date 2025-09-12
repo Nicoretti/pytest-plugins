e1acd3850719367f009de2aacf7bca7cc19ea14b
Nicola Coretti <nico.coretti@gmail.com>
2025-09-12 17:31:50 +0200

    Add context manager support

--- src/pytest_artifacts/__init__.py
+++ src/pytest_artifacts/__init__.py
@@ -3,14 +3,32 @@ from __future__ import annotations
 from datetime import datetime
 from pathlib import Path
 import shutil
-
 import pytest
+import logging
+from contextlib import contextmanager
+
+_logger = logging.getLogger(__name__)
 
 
 class Storage:
 
-    def __init__(self, path):
+    def __init__(self, path, parent=None):
         self._path = path
+
+    @property
+    def path(self):
+        return self._path
+
+    @contextmanager
+    def open(self, path, mode='w'):
+        path = self.path / path
+        path.parent.mkdir(exist_ok=True)
+        file = open(path, mode)
+        try:
+            yield file
+        finally:
+            if file:
+                file.close()
 
     def save(self, file: Path):
         file = Path(file)
@@ -19,15 +37,24 @@ class Storage:
             dest.mkdir(parents=True, exist_ok=True)
         else:
             dest.parent.mkdir(parents=True, exist_ok=True)
+        _logger.debug(f"Saving artifact '%s' to '%s'", file, dest)
         shutil.copy(file, dest)
 
     def sub_storage(self, path):
-        return Storage(self._path / path)
+        return Storage(self._path / path, parent=self)
 
 
 @pytest.fixture(scope="session")
-def session_storage(request):
-    yield Storage(request.config.option.artifacts_path)
+def session_storage(request, tmp_path_factory):
+    storage_path = tmp_path_factory.mktemp("storage")
+    _logger.debug("Creating storage folder '%s'", storage_path)
+
+    yield Storage(storage_path)
+
+    name = f"Test-Artifacts-{datetime.now()}"
+    archive = shutil.make_archive(
+        base_name=name, format="zip", root_dir=storage_path)
+    _logger.debug("Storing compressed artifacts in '%s'", archive)
 
 
 @pytest.fixture
@@ -37,15 +64,15 @@ def storage(request, session_storage):
 
 def pytest_addoption(parser: pytest.Parser):
     artifacts = parser.getgroup("artifacts")
-    default_storage = Path.cwd() / f"{datetime.now()}"
+    path = Path.cwd() / f"{datetime.now()}"
     artifacts.addoption(
         "--artifacts-path",
         type=Path,
-        default=default_storage,
-        help=f"Path to store the artifacts [default: {default_storage}]"
+        help=f"Path to store the artifacts zip to [default: {path}]"
     )
-
-
-def pytest_configure(config: pytest.Config):
-    artifact_path = config.option.artifacts_path
-    artifact_path.mkdir(exist_ok=True)
+    artifacts.addoption(
+        "--artifacts-archive-format",
+        choices=["zip", "tar.gz"],
+        default="zip",
+        help="Path to store the artifacts zip to"
+    )
--- test/some_test.py
+++ test/some_test.py
@@ -1,8 +1,29 @@
-def test_something(storage):
-    name = "somefile.txt" 
-    with open(name, 'w') as f:
-        for i in range(0,100):
+import pytest
+
+
+@pytest.fixture
+def testfile(tmp_path):
+    file = tmp_path / "testfile.txt"
+    with open(file, "w") as f:
+        for i in range(0, 3):
             f.write(f"Line {i}\n")
-    storage.save(name)
+    yield file
+
+
+def test_something(storage, testfile):
+    storage.save(testfile)
     assert True
 
+
+def test_something2(storage):
+    from inspect import cleandoc
+    with storage.open("somefile2.txt", "w") as f:
+        content = cleandoc(
+            """
+            Line 1
+            Line 2
+            Line 3
+            """
+        )
+        f.write(content)
+    assert True
